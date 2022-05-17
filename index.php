b6de1d4f050518b2233282f1c51bf174537cdb87
RickRietbergen <rbps3.2016@gmail.com>
2022-05-17 22:15:24 +0200

    header done

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <!-- link css -->
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- tab logo page -->
+    <!-- google fonts -->
+</head>
+    <?php
+        include_once "php/header.php";
+    ?>
+<body>
+    
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,27 @@
+<header>
+  <nav>
+    <ul>
+      <li>
+        <a href="#" class="txt_logo">
+          <p>TRAVELEGY</p>
+        </a>
+      </li>
+      <li class="links">
+        <ul>
+          <li id="txt_header" class="li_header">
+            <a href="#">Home</a>
+          </li>
+          <li class="li_header">
+            <a href="#">About</a>
+          </li>
+          <li class="li_header">
+            <a href="#">Projects</a>
+          </li>
+          <li class="li_header">
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</header>
