e3bbe31c57e4ce938796ed6bdffa32acb86b7afc
RickRietbergen <rbps3.2016@gmail.com>
2022-05-24 22:04:03 +0200

    only add txt

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,29 @@
 <footer>
     <div class="icons">
-        
+        <div class="icon">
+            <img src="assets/afbeeldingen/twitter.png" alt="twitter logo">
+        </div>
+
+        <div class="icon">
+            <img src="assets/afbeeldingen/linkedin.png" alt="twitter logo">
+        </div>
+
+        <div class="icon">
+            <img src="assets/afbeeldingen/github.png" alt="twitter logo">
+        </div>
+
+        <div class="icon">
+            <img src="assets/afbeeldingen/instagram.png" alt="twitter logo">
+        </div>
+
+        <div class="icon" id="icon_end">
+            <img src="assets/afbeeldingen/gmail.png" alt="twitter logo">
+        </div>
+    </div>
+
+    <div class="copyright">
+        <div class="info">
+            <p>test -- test</p>
+        </div>
     </div>
 </footer>
